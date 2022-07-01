50f33062ea9584e5a40f57939aa237d2e08f739f
Saloni Jindal <salonijindal@MacBook-Pro.ht.home>
2022-07-01 00:07:04 -0400

    Html Tag for Video description is added. Informations as placehoolder is added with styling of mobile screen

--- src/components/HeroVideo/HeroVideo.js
+++ src/components/HeroVideo/HeroVideo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./HeroVideo.scss";
 
 const HeroVideo = () => {
   return (
@@ -8,25 +9,29 @@ const HeroVideo = () => {
         poster="https://i.imgur.com/l2Xfgpl.jpg"
         controls
       ></video>
-      <h1>Song name</h1>
-      <div>
+      <div className="hero-section__description">
+        <h1>MX Rampage: 2021 Highlights</h1>
+        <hr></hr>
         <div>
-          <h2>Singer name</h2>
-          <p className="">1,001,023</p>
+          <div className="hero-section__info">
+            <h2>By Red Crow</h2>
+            <p className="">1,001,023</p>
+          </div>
+          <div className="hero-section__info">
+            <p>07/11/2021</p>
+            <p>1,001,023</p>
+          </div>
+          <hr></hr>
+          <p>
+            On a gusty day in Southern Utah, a group of 25 daring mountain
+            bikers blew the doors off what is possible on two wheels, unleashing
+            some of the biggest moments the sport has ever seen. While mother
+            nature only allowed for one full run before the conditions made it
+            impossible to ride, that was all that was needed for event veteran
+            Kyle Strait, who won the event for the second time -- eight years
+            after his first Red Cow Rampage title
+          </p>
         </div>
-        <div>
-          <p>07/11/2021</p>
-          <p>1,001,023</p>
-        </div>
-        <p>
-          On a gusty day in Southern Utah, a group of 25 daring mountain bikers
-          blew the doors off what is possible on two wheels, unleashing some of
-          the biggest moments the sport has ever seen. While mother nature only
-          allowed for one full run before the conditions made it impossible to
-          ride, that was all that was needed for event veteran Kyle Strait, who
-          won the event for the second time -- eight years after his first Red
-          Cow Rampage title
-        </p>
       </div>
     </div>
   );
